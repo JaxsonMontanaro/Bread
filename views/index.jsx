212207e97f9280e9934446a9b01eae8ea8b9f6d7
JaxsonMontanaro <jaxsonmontanaro@gmail.com>
2023-02-23 16:03:30 -0800

    sending index and show data

---
+++ views/index.jsx
@@ -0,0 +1,35 @@
+const React = require('react')
+const Default = require('./layouts/Default')
+
+function Index ({breads})  {
+    return (
+<Default>
+  <h2>Index Page</h2>
+  {
+  breads.map((bread, index)=> {
+    return (
+      <li key={index}>
+        <a href={`/breads/${index}`}>
+          {bread.name}
+        </a>
+      </li>
+    )
+  })
+}
+
+  <ul>
+    {
+      breads.map((bread, index)=> {
+        return (<li></li>)
+      }) 
+    }
+  </ul>
+</Default>
+
+
+
+    )
+}
+
+
+module.exports = Index
